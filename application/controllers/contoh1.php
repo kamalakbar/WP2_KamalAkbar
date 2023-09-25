0e8fa5f5b80898aabec77d0aa5c76cbf7faca44c
kamalakbar <muhamadkamal275@gmail.com>
2023-09-26 03:17:16 +0700

    menambah pustaka booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama saya kamal 
+              saya tinggal di jakarta barat
+              hobby saya badminton  ";
+
+    }
+}
