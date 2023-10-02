c43c29a60df68b6f52f136a8e563b42ab8721f00
kamalakbar <muhamadkamal275@gmail.com>
2023-10-02 16:55:55 +0700

    pertemuan 3 mvc

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,10 @@
+<html>
+    <head>Latihan 1</head>
+
+    <body>
+        Halo boss Welcome to indomaret<br>
+        Nilai 1 = <?= $nilai1; ?>
+        Nilai 2 = <?= $nilai2; ?>
+        ini hasil dari permodelan dengan methode penjumlahan yaitu<?=$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+    </body>
+</html>
